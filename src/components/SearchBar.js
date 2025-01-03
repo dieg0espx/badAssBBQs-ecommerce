bda10f1fcbee03158c1cafeb6ce205402677d4fa
dieg0espx <espinosa9mx@gmail.com>
2025-01-03 14:58:51 +0100

    .

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useProducts } from '../context/ProductsContext';
+import placeholderImage from '../images/placeholder_image.jpg'
+
 
 function SearchBar() {
   const { loadAllProducts } = useProducts();
@@ -19,9 +21,14 @@ function SearchBar() {
   useEffect(() => {
     if (finding !== '') {
       setShowList(true);
-      const filteredProducts = products.filter(product =>
-        product.Title.toLowerCase().includes(finding.toLowerCase())
-      );
+      // const filteredProducts = products.filter(product =>
+      //   product.Title.toLowerCase().includes(finding.toLowerCase())
+      // );
+      const filteredProducts = products.filter(product => {
+        const keywords = finding.toLowerCase().split(' ').filter(word => word); // Split the search input into words
+        return keywords.every(keyword => product.Title.toLowerCase().includes(keyword)); // Check if all keywords are present
+      });
+      
       setResults(filteredProducts)
       console.log("Filtered Products:", filteredProducts); // Print filtered products in the console
     } else {
@@ -39,9 +46,7 @@ function SearchBar() {
         setShowList(false);
       }
     };
-
     window.addEventListener('scroll', handleScroll);
-
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -59,7 +64,15 @@ function SearchBar() {
         <div className='fixed bg-white w-[90%] xl:max-w-[485px] border border-gray-200 max-h-[300px] rounded-b border-t-0 overflow-y-scroll'>
          {results.map((result, index) => (
             <div key={index} className='grid grid-cols-[50px_auto_10px] gap-[20px] p-[10px] border-t border-gray-200 items-center align-center text-gray-500 hover:text-red' onClick={()=>handleClick(result.brand, result.Id)}>
-                <img src={result.Image}  loading="lazy" className='w-[50px] h-[50px]' />
+                <img 
+                  src={result.Image}  
+                  loading="lazy" 
+                  className='w-[50px] h-[50px]' 
+                  onError={(e) => {
+                    e.target.onerror = null; // Prevent infinite loop in case placeholder also fails
+                    e.target.src = placeholderImage
+                  }}
+                />
                 <p> {result.Title} </p>
                 <i class="bi bi-chevron-compact-right"></i>
             </div>
